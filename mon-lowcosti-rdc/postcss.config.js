81a753e582aeeba7b3c923b453939a4a6069c32f
Mylze00 <134407750+Mylze00@users.noreply.github.com>
2025-07-03 06:47:01 +0100

    Delete mon-lowcosti-rdc directory

--- mon-lowcosti-rdc/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// postcss.config.js
-export default {
-  plugins: {
-    // Ancien: tailwindcss: {},
-    // Nouveau: Utilisez le plugin `@tailwindcss/postcss`
-    '@tailwindcss/postcss': {}, // C'est la ligne à changer/ajouter
-    autoprefixer: {},
-  },
-}
